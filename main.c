37dd6ad098e188b00fcce2330c921d5e1280433b
rednayan <nayansharma1010@gmail.com>
2023-07-25 12:33:05 +0530

    feat: push play/stop

--- main.c
+++ main.c
@@ -1,14 +1,34 @@
 #include <raylib.h>
 #include <stdio.h>
 
+void callback(void *bufferData, unsigned int frames) {
+  (void)bufferData;
+  printf("callback(frames = %u)\n", frames);
+}
+
 int main(void) {
   InitWindow(800, 400, "raymuse");
+  InitAudioDevice();
+  SetTargetFPS(60);
+
   Music music = LoadMusicStream("../test-music.mp3");
+  PlayMusicStream(music);
 
   while (!WindowShouldClose()) {
+    UpdateMusicStream(music);
+    AttachAudioStreamProcessor(music.stream, callback);
+
+    if (IsKeyDown(KEY_SPACE)) {
+      if (IsMusicStreamPlaying(music)) {
+        PauseMusicStream(music);
+      } else {
+        ResumeMusicStream(music);
+      }
+    }
+
     BeginDrawing();
     ClearBackground(WHITE);
-    PlayMusicStream(music);
     EndDrawing();
   }
+  return 0;
 }
